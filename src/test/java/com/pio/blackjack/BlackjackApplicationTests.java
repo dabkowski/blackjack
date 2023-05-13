3f2d6a375b366846cc0ae0c44a6bb60e7cf63913
verion <bijacik.adam@gmail.com>
2023-05-13 19:31:31 +0200

    Create empty project

---
+++ src/test/java/com/pio/blackjack/BlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pio.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
