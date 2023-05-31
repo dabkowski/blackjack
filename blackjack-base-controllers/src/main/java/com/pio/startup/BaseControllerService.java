ade3da7413d0dff5af8c68eaf51da21a64b64c37
Wikcis <wiktor.ciszewski7734@gmail.com>
2023-06-01 00:15:13 +0200

    Delete game screen to update it

--- blackjack-base-controllers/src/main/java/com/pio/startup/BaseControllerService.java
+++ blackjack-base-controllers/src/main/java/com/pio/startup/BaseControllerService.java
@@ -13,7 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.*;
 import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
